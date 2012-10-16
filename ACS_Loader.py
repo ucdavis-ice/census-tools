150b08d026e5ddf0fa32af0ded403f9ed64c4cb8
Nathaniel Roth <neroth@ucdavis.edu>
2012-10-15 17:13:32 -0700

    Add licensing
    Signed-off-by: Nathaniel Roth <neroth@ucdavis.edu>

--- ACS_Loader.py
+++ ACS_Loader.py
@@ -2,6 +2,21 @@
 Created on Oct 15, 2012
 
 @author: roth
+
+Copyright 2012 Nathaniel Roth, University of California, Davis
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
 '''
 
 
@@ -385,11 +400,9 @@ if __name__ == "__main__":
     mst.LoadSequences(schema, "20105ca0", "F:\\Census\\ACS_2006_2010\\BG_Tracts")
     mst.LoadSequences("acs_2010_5", "20105ca0", "F:\\Census\\ACS_2006_2010\\All_OtherGeom")
     mst.MakeIndexes(schema)
-    
-    
+        
     mst.CloseConnection()
     mst.CloseLog()
-    print "Done" 
     
     # Loading Geography Tables
     mgt = MakeGeoTable()
